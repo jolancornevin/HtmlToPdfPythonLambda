7d44646d565321572774a958988fc81892fb38a6
Jolan Cornevin <jcornevin@work4labs.com>
2018-09-06 18:54:09 +0200

    update lambda function to handle no url param given

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -3,8 +3,20 @@ import json
 from webdriver_wrapper import WebDriverWrapper
 
 
+def error(title):
+    return json.dumps({
+        "isBase64Encoded": False,
+        "statusCode": 403,
+        "body": title
+    })
+
+
 def lambda_handler(event, context):
-    url = event.get('queryStringParameters', {}).get('url', 'https://www.google.com/')
+    url = event.get('queryStringParameters', {}).get('url')
+
+    if not url:
+        return error("You must provide a GET url parameter")
+
     print('Received url {} to parse'.format(url))
 
     driver = WebDriverWrapper()
